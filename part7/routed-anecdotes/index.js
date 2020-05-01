93c106106d42001ab531b47768c5eab98feb0ebf
Abhay Desai <47878872+abhaydesai76@users.noreply.github.com>
2020-05-01 12:29:08 +0400

    Add files via upload

--- part7/routed-anecdotes/index.js
+++ part7/routed-anecdotes/index.js
@@ -1,1 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+// import RouterAppBootstrap from './RouterAppBootstrap'
+// import RouterAppMaterial from './RouterAppMaterial'
+import RouterAllStyled from './RouterAppStyled'
+import { BrowserRouter as Router } from 'react-router-dom'
 
+ReactDOM.render
+(<Router>
+    <App />
+    {/* <RouterAppBootstrap /> */}
+    {/* <RouterAllStyled /> */}
+</Router>, 
+document.getElementById('root'))
