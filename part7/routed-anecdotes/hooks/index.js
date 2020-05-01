c6c0bbde649fc416e4e67c5a2b126ec7385de219
Abhay Desai <47878872+abhaydesai76@users.noreply.github.com>
2020-05-01 12:30:02 +0400

    Add files via upload

--- part7/routed-anecdotes/hooks/index.js
+++ part7/routed-anecdotes/hooks/index.js
@@ -1,1 +1,33 @@
+import { useState } from 'react'
 
+export const useCounter = () => {
+    const [value, setValue] = useState(0)
+
+    const increase = () => {
+        setValue(value +1)
+    }
+
+    const decrease = () => {
+        setValue(value -1)
+    }
+
+    const zero = () => {
+        setValue(0)
+    }
+
+    return {value, increase, decrease, zero}
+}
+
+export const useField = (type) => {
+    const [value, setValue] = useState('')
+
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    const onReset = (event) => {
+        setValue('')
+    }
+
+    return { type, value, onChange, onReset}
+}
